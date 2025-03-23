e55e4a5aa6567af4963ae78c05ce44155984310f
cinda87 <j.c.bietz@gmail.com>
2025-03-23 14:18:20 -0600

    Updated footer styling

--- my-vite-app/src/Components/Footer.jsx
+++ my-vite-app/src/Components/Footer.jsx
@@ -1,32 +1,36 @@
 import React from "react";
+import "../Style/Footer.css"; 
 
 export default function Footer() {
   return (
-    <footer className="text-center">
-      This project was{" "}
-      <a
-        href="https://www.linkedin.com/in/jacinda-bietz-3158a0338/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        coded by Jacinda Bietz
-      </a>{" "}
-      is{" "}
-      <a
-        href="https://github.com/CindaCodes/React-Weather-Forecast"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        open-sourced on GitHub
-      </a>{" "}
-      and is{" "}
-      <a
-        href="https://cindas-weather-forecast.netlify.app/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        hosted on Netlify.
-      </a>
+    <footer className="custom-footer">
+      <p>
+        This project was{" "}
+        <a
+          href="https://www.linkedin.com/in/jacinda-bietz-3158a0338/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          coded by Jacinda Bietz
+        </a>
+        , is{" "}
+        <a
+          href="https://github.com/CindaCodes/React-Weather-Forecast"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://cindas-weather-forecast.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+        .
+      </p>
     </footer>
   );
 }
